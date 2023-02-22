9adf5141c91d648aba23e10b3a5660530556eaf7
Felipe Simões <ffelipesimoes@gmail.com>
2023-02-22 19:57:26 -0300

    chore: add utils file

---
+++ api/utils.js
@@ -0,0 +1,18 @@
+const getUserId = (headers) => {
+  return headers.app_user_id
+}
+const getUserName = (headers) => {
+  return headers.app_user_name
+}
+
+const getResponseHeaders = () => {
+  return {
+    'Access-Control-Allow-Origin': '*'
+  }
+
+module.exports = {
+  getResponseHeaders,
+  getUserId,
+  getUserName
+}
+}
